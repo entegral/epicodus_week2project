2c6b60054ea9d24d0f01c4d0696d9a15800140af
Rob <e2robby@gmail.com>
2018-10-19 12:43:05 -0700

    begin to add back-end logic to record and tally votes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,31 @@
-// personal-info variables
+// personal-info and preferences variables
 var name;
-var question1;
-var question2;
-var question3;
-var question4;
-var question5;
 var companySize;
+var versatility;
+var newOrOld;
+var frontOrBackEnd;
+var interest;
 
-
+// tally of points for language options
+var cSharpTally;
+var javaTally;
+var phpTally;
+var rubyTally;
 
 // back-end calculation logic
+var addVote = function(selection, question) {
+  if (question === 1) {
 
+  } else if (question === 2) {
+
+  } else if (question === 3) {
+
+  }else if (question === 4) {
+
+  }else if (question === 5) {
+
+  }
+}
 
 
 $(function() {
@@ -30,35 +45,42 @@ $(function() {
 
   $('form#preferences-form-1').submit(function(event) {
     event.preventDefault();
-    companySize = $("#company-size").val()
-    if (companySize) {
-      $('#preferences-1').addClass("hidden");
-      $('#preferences-2').removeClass("hidden");
-    } else {
-      alert("Please input your a preferred size before proceeding.")
-    }
+    companySize = $("#company-size").val();
+    addVote(companySize, 1);
+    $('#preferences-1').addClass("hidden");
+    $('#preferences-2').removeClass("hidden");
   });
 
   $('form#preferences-form-2').submit(function(event) {
     event.preventDefault();
-    versatility = $("#company-size").val()
-    if (companySize) {
-      $('#preferences-2').addClass("hidden");
-      $('#preferences-3').removeClass("hidden");
-    } else {
-      alert("Please input versatility preference before submitting.")
-    }
+    versatility = $("#versatility").val();
+    addVote(versatility, 2);
+    $('#preferences-2').addClass("hidden");
+    $('#preferences-3').removeClass("hidden");
   });
 
   $('form#preferences-form-3').submit(function(event) {
     event.preventDefault();
-    versatility = $("#company-size").val()
-    if (companySize) {
-      $('#preferences-3').addClass("hidden");
-      $('#preferences-4').removeClass("hidden");
-    } else {
-      alert("Please input versatility preference before submitting.")
-    }
+    newOrOld = $("#newOrOld").val();
+    addVote(newOrOld, 3)
+    $('#preferences-3').addClass("hidden");
+    $('#preferences-4').removeClass("hidden");
+  });
+
+  $('form#preferences-form-4').submit(function(event) {
+    event.preventDefault();
+    frontOrBackEnd = $("#frontOrBackEnd").val();
+    addVote(frontOrBackEnd, 4)
+    $('#preferences-4').addClass("hidden");
+    $('#preferences-5').removeClass("hidden");
+  });
+
+  $('form#preferences-form-5').submit(function(event) {
+    event.preventDefault();
+    interest = $("#interest").val();
+    addVote(interest, 5)
+    $('#preferences-5').addClass("hidden");
+    $('#track').removeClass("hidden");
   });
 
 
