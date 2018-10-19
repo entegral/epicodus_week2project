55565094fdb77a11f00ba6fe35ae58d820217e9e
Rob <e2robby@gmail.com>
2018-10-19 10:00:38 -0700

    add html content and jquery vars, selectors and functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
+// personal-info variables
+var name;
+
+// preference variables
+
+
+
+// back-end calculation logic
+
+
+
 $(function() {
+  $("preferences").hide();
+  $("suggestion").hide();
+
+  // personal info variale assignment
+  $("form#personal-info").submit(function() {
+    name = $("input#name").val();
+    $("#personal-info-panel").hide();
+  });
 
 
 
