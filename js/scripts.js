601e470c9d5a3bce6974f022ea1f797ecc8ea323
Rob <e2robby@gmail.com>
2018-10-19 11:46:24 -0700

    add additional questions and jquery logic based on language options

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ var question2;
 var question3;
 var question4;
 var question5;
+var companySize;
 
 
 
@@ -21,18 +22,44 @@ $(function() {
 
     if (name) {
       $('#personal').addClass("hidden");
-      $('#preferences').removeClass("hidden");
+      $('#preferences-1').removeClass("hidden");
     } else {
       alert("Please input your name before proceeding.")
     }
   });
 
-  $('form#preferences-form').submit(function(event) {
+  $('form#preferences-form-1').submit(function(event) {
     event.preventDefault();
-    var companySize = $("#company-size").val()
-
-
-
+    companySize = $("#company-size").val()
+    if (companySize) {
+      $('#preferences-1').addClass("hidden");
+      $('#preferences-2').removeClass("hidden");
+    } else {
+      alert("Please input your a preferred size before proceeding.")
+    }
   });
+
+  $('form#preferences-form-2').submit(function(event) {
+    event.preventDefault();
+    versatility = $("#company-size").val()
+    if (companySize) {
+      $('#preferences-2').addClass("hidden");
+      $('#preferences-3').removeClass("hidden");
+    } else {
+      alert("Please input versatility preference before submitting.")
+    }
+  });
+
+  $('form#preferences-form-3').submit(function(event) {
+    event.preventDefault();
+    versatility = $("#company-size").val()
+    if (companySize) {
+      $('#preferences-3').addClass("hidden");
+      $('#preferences-4').removeClass("hidden");
+    } else {
+      alert("Please input versatility preference before submitting.")
+    }
+  });
+
 
 });
